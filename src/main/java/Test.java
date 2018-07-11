dc59f2c1b98b01e33165cdb0e6de2fe0d19143e4
fl <1953927885@qq.com>
2018-07-11 17:50:16 +0800

    di yi 1 次

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,7 +3,13 @@
  */
 public class Test {
     public static void main(String[] args) {
+
         System.out.println("测试主节点 master");
+    }
+
+    public void test(){
+
+        System.out.println("第一次提交");
     }
 
 
